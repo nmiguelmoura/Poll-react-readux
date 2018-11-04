e360ff55e481c6bc7d23e80a209ed38cb057dc8b
nmiguelmoura <nmiguelmoura@gmail.com>
2018-11-04 21:40:50 +0000

    type: feat Get initial data.

--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -1,1 +1,8 @@
 export const SET_AUTHED_USER = 'SET_AUTHED_USER';
+
+export function setAuthenticatedUser(id) {
+    return {
+        type: SET_AUTHED_USER,
+        id
+    }
+}
--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,19 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import { handleInitialData } from "../actions/shared";
 
+import { setAuthenticatedUser } from "../actions/authedUser";
+
 class App extends Component {
     componentDidMount() {
         this.props.dispatch(handleInitialData());
+
+
+
+
+        //TESTINNG AREA
+        setTimeout(() => {
+            this.props.dispatch(setAuthenticatedUser('sarahedo'));
+        }, 1000);
     }
 
     render() {
